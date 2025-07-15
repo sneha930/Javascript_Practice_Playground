eafceb225bbd380885fd9df3a261d8a115eb4e69
sneha <sangaonkarsneha@gmail.com>
2025-07-15 19:23:21 +0530

    Day 1: Variables (var, let, const) & Data Types

---
+++ App.js
@@ -0,0 +1,25 @@
+var name = "sneha";
+var name = "Sneha";
+
+let city = "Nagpur";
+city = "Mumbai"
+
+const age = 22;
+
+console.log(name);
+console.log(city);
+console.log(age);
+
+let fruit = "Mango";
+const year = 2025;
+var isRipe = true;
+
+console.log(typeof fruit); // "string"
+console.log(typeof year);  // "number"
+console.log(typeof isRipe); // "boolean"
+
+let test;
+console.log(typeof test); // "undefined"
+
+let empty = null;
+console.log(typeof empty); // "object" (quirk!)
