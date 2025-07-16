c1cb8a8d59e1fa781b61c4ddaa119be85edf35a6
sneha <sangaonkarsneha@gmail.com>
2025-07-16 18:07:17 +0530

    Day 2: Functions (declaration, expression, arrow)

--- App.js
+++ App.js
@@ -1,25 +1,36 @@
-var name = "sneha";
-var name = "Sneha";
+//  Task 1: Write a function declaration add that adds 2 numbers.
+function addF(num1, num2) {
+   return num1+num2;
+}
+console.log(addF(2, 3));
 
-let city = "Nagpur";
-city = "Mumbai"
+// Task 2: Rewrite it as a function expression.
+const addE = function(num1, num2) {
+   return num1+num2;
+}
+console.log(addE(6, 6));
 
-const age = 22;
+// Task 3: Rewrite it as an arrow function.
+const addA = (num1, num2) => {
+   return num1+num2;
+}
+console.log(addA(2, 1));
 
-console.log(name);
-console.log(city);
-console.log(age);
+// Task 4: Write an arrow function with implicit return
+const addI = (num1, num2) => num1+num2;
+console.log(addI(2, 1));
 
-let fruit = "Mango";
-const year = 2025;
-var isRipe = true;
+// Task 5: Write an arrow function that returns an object.
+const createUser = (name, age) => ({name, age});
 
-console.log(typeof fruit); // "string"
-console.log(typeof year);  // "number"
-console.log(typeof isRipe); // "boolean"
-
-let test;
-console.log(typeof test); // "undefined"
-
-let empty = null;
-console.log(typeof empty); // "object" (quirk!)
+const obj = {
+   value: 42,
+   normalFunc: function() {
+      console.log(this.value);
+   },
+   arrowFunc: () => {
+      console.log(this.value);
+   }
+}
+obj.normalFunc();
+// obj.arrowFunc();
