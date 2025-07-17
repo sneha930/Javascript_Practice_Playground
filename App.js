9ff3c0b645b74a022a6e39d32bea4759890a587e
sneha <sangaonkarsneha@gmail.com>
2025-07-17 17:59:50 +0530

    Day 6: JSON & Fetch basics

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
-// ✅ Task: Write a function checkNumber that takes a number: If it’s even, log “Even number” Else, log “Odd number”.
+// ✅ Task 1: JSON Write an object book with: title, author, year Convert it to JSON Parse it back to an object Log the title.
 
-function checkNumber(num) {
-  if(num%2 == 0) {
-    console.log("Even number");
-  }
-  else {
-    console.log("Odd number");
-  }
+const book = {
+  title: "The Alchemist",
+  author: "Paulo coelho",
 }
-checkNumber(2);
-checkNumber(3);
+const jsonString = JSON.stringify(book);
+console.log(jsonString);
 
-// Task: Write a for loop that prints only even numbers from 1 to 10.
-function printEvenNumbers() {
-  for(let i=1; i<=10; i++) {
-    if(i%2==0) console.log(i);
-  }
-}
-printEvenNumbers();
+const parsed = JSON.parse(jsonString);
+console.log(parsed);
 
-// Task: Write a while loop that counts down from 5 to 1.
-let i = 5;
-while(i>=1) {
-  console.log(i);
-  i--;
-}
+// ✅ Task 2: Fetch: Use fetch to GET data from: https://jsonplaceholder.typicode.com/users
+// Get the JSON response. Log the first user’s name.
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response => response.json()) 
+  .then(data => console.log(data[0].name)); 
 
-// Task: Write a for...of loop that loops through an array of fruits and logs each fruit in uppercase.
-const fruits = ["apple", "banana", "mango", "strawberry"];
-for(const fruit of fruits) {
-  console.log(fruit.toUpperCase());
-}
+
+// ✅ Task 3: POST a new post to https://jsonplaceholder.typicode.com/posts
+// Data: { title: 'Test', body: 'This is a test.', userId: 101 Log the response.
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Test",
+    body: "This is a test.",
+    userId: 101
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
+
+
