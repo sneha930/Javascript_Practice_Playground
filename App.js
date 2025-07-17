6b18794885b84f8a309f950339a5ca1f2489a997
sneha <sangaonkarsneha@gmail.com>
2025-07-17 16:40:17 +0530

    Day 5: Conditionals, Loops (for, while, for...of)

--- App.js
+++ App.js
@@ -1,29 +1,33 @@
-// Template Literals
-// Task: greet function with name & day
-function greet(name, day) {
-  return `Hello, ${name}! Have a great ${day}!`;
+// ✅ Task: Write a function checkNumber that takes a number: If it’s even, log “Even number” Else, log “Odd number”.
+
+function checkNumber(num) {
+  if(num%2 == 0) {
+    console.log("Even number");
+  }
+  else {
+    console.log("Odd number");
+  }
 }
-console.log(greet("Sneha", "Monday")); 
+checkNumber(2);
+checkNumber(3);
 
-
-// Default Parameters
-// Task: order function with default quantity = 1
-function order(item, quantity = 1) {
-  console.log(`You ordered ${quantity} ${item}.`);
+// Task: Write a for loop that prints only even numbers from 1 to 10.
+function printEvenNumbers() {
+  for(let i=1; i<=10; i++) {
+    if(i%2==0) console.log(i);
+  }
 }
-order("Coffee");  
-order("Tea", 2);  
+printEvenNumbers();
 
-// Spread Operator
-// Task: Merge two arrays of fruits
-const fruits1 = ["Apple", "Banana"];
-const fruits2 = ["Mango", "Orange"];
-const allFruits = [...fruits1, ...fruits2];
-console.log(allFruits); 
-
-// Rest Operator
-// Task: logNames function with rest operator
-function logNames(...names) {
-  console.log("Names:", names);
+// Task: Write a while loop that counts down from 5 to 1.
+let i = 5;
+while(i>=1) {
+  console.log(i);
+  i--;
 }
-logNames("Sneha", "Rahul", "Amit");  
+
+// Task: Write a for...of loop that loops through an array of fruits and logs each fruit in uppercase.
+const fruits = ["apple", "banana", "mango", "strawberry"];
+for(const fruit of fruits) {
+  console.log(fruit.toUpperCase());
+}
